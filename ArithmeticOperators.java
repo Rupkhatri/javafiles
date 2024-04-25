95fd960a113d9aff9df3fa0bb1b81ba73300c55a
Rupkhatri <167824219+Rupkhatri@users.noreply.github.com>
2024-04-25 13:56:25 +0100

    Add files via upload
    Homework

---
+++ ArithmeticOperators.java
@@ -0,0 +1,15 @@
+package arithmetic;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int A= 20;
+
+        int B = 30;
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+
+    }
+}
